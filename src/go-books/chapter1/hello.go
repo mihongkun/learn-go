9697e5f588f04a3220dc4dd472199b77761a4486
mihk <mihongkun@gmail.com>
2017-08-29 16:16:09 +0800

    add type

--- src/go-books/chapter1/hello.go
+++ src/go-books/chapter1/hello.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main(){
+	fmt.Println("1 + 1 = ", 1 + 1)
+	fmt.Println("1 + 1 = ", 1.0 + 1.0)
+	fmt.Println("Hello World"[1])
+	fmt.Println(len("Hello World"))
+	fmt.Println("Hello" + " World")
+	fmt.Println("1 / 2 = ", 1.0 / 2.0)
 	fmt.Println("Hello World")
 }
+
