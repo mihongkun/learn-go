167df0b05f5e09ca21113ca5e30a42b6082cf0a9
mihk <mihongkun@gmail.com>
2017-08-31 16:58:31 +0800

    add something

---
+++ src/go-books/chapter1/func_defer.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main(){
+	defer second()
+	first()	
+}
+
+func first(){
+	fmt.Println("1st")
+}
+
+
+func second(){
+	fmt.Println("2nd")
+}
+
---
+++ src/go-books/chapter1/func_recursion.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println(factorial(20))	
+}
+
+func factorial(x uint) uint {
+	if x == 0 {
+		return 1
+	}
+	return x * factorial(x - 1)
+}
+
+
+
+
